f88f315aed56acd046f7766006ec3e7394e36eaa
MatthewRobot <147113127+MatthewRobot@users.noreply.github.com>
2025-10-18 22:45:39 -0400

    compiled lib for web

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart'; // NEW: Firebase Auth Import
+import 'package:firebase_auth/firebase_auth.dart';
 import 'providers/game_provider.dart';
 import 'screens/Entrance.dart';
 import '../services/auth_service.dart';
